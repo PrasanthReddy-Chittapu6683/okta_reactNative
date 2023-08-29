9f9e68b09ba434b0ac4a960e9a488631fbf2a896
Prasanth Reddy CV <67952842+PrasanthReddy-Chittapu6683@users.noreply.github.com>
2023-08-29 00:28:04 -0600

    Add files via upload

---
+++ auth.config.ts
@@ -0,0 +1,10 @@
+export default {
+  oidc: {
+    clientId: '0oab05a0eavoP0Ox75d7',
+    redirectUri: 'com.okta.dev-70352772:/callback',
+    endSessionRedirectUri: 'com.okta.dev-70352772:/logout',
+    discoveryUri: 'https://dev-70352772.okta.com/oauth2/default',
+    scopes: ['openid', 'profile', 'offline_access'],
+    requireHardwareBackedKeyStore: false,
+  },
+};
